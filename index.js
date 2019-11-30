ae0b5e67623ebf85d3c07ef0aafd8e43289e74c2
bbibbibiq <peerasorn14@gmail.com>
2019-12-01 00:55:20 +0700

    Add auth with firebase

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const basicAuth = require('./lib/basic-auth')
-const tokenAuthRouter = require('./lib/token-auth')
-const allUserRouter = require('./lib/all-users')
-const findUser = require('./lib/find-user')
-const tokenRouter = require('./routes/tokens')
+// const basicAuth = require('./lib/basic-auth')
+// const tokenAuthRouter = require('./lib/token-auth')
+// const allUserRouter = require('./lib/all-users')
+// const findUser = require('./lib/find-user')
+// const tokenRouter = require('./routes/tokens')
+const fireAuth = require('./lib/fire-auth')
 
 const express = require('express')
 const app = express()
 
 
-app.use('/tokens', tokenRouter)
-app.use(tokenAuthRouter)
-app.use(allUserRouter)
-app.use(basicAuth(findUser.byCredentials))
+app.use(fireAuth)
+// app.use('/tokens', tokenRouter)
+// app.use(tokenAuthRouter)
+// app.use(allUserRouter)
+// app.use(basicAuth(findUser.byCredentials))
 
 app.listen(3000)
-
